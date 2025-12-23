d0d7e02f68e7501668ec068a91e9b5c5dacc2c18
SeNpAi2407 <darkpantera786@gmail.com>
2025-12-23 23:54:14 +0100

    first init

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,36 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
+    static double result = 0;
 
     public static void main(String[] args) {
-        //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор
-        a = inputDouble();
-        b = inputDouble();
+        while(true) {
+            a = inputDouble();
+            b = inputDouble();
 
-        sum();      //Метод суммирования чисел
-        multiply(); //Метод умножения чисел
-        diff();     //Метод вычитания чисел
-        divide();   //Метод деления чисел
+            System.out.println("Введите необходимое действие:\n 1 - суммированиe чисел\n 2 - умножениe чисел\n 3 - вычитаниe чисел\n 4 - делениe чисел\n 5 - выход");
+
+            double choise = inputDouble();
+            if (choise == 1) {
+                sum();
+            } else if (choise == 2) {
+                multiply();
+            } else if (choise == 3) {
+                diff();
+            } else if (choise == 4) {
+                divide();
+            } else if (choise == 5) {
+                break;
+            }
+        }
     }
 
     private static void divide() {
-        //todo дописать логику и печать результата
+        result = a / b;
+        System.out.println("Результат :" + result);
     }
 
     private static void diff() {
-        //todo дописать логику и печать результата
+        result = a - b;
+        System.out.println("Результат :" + result);
     }
 
     private static void multiply() {
-        //todo дописать логику и печать результата
+        result = a * b;
+        System.out.println("Результат :" + result);
     }
 
     private static void sum() {
-        //todo дописать логику и печать результата
+        result = a + b;
+        System.out.println("Результат :" + result);
     }
 
     private static double inputDouble() {
-        //todo дописать логику
-        return 0;
+        System.out.println("Введите число:");
+        return new Scanner(System.in).nextDouble();
     }
 }
